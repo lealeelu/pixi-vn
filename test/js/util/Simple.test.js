a1b00707b8763a14146f28bc2a2dfde292cf9134
lealeelu <lmb@iu.edu>
2017-08-12 23:40:01 -0400

    Change tests to function() fixes #14

--- test/js/util/Simple.test.js
+++ test/js/util/Simple.test.js
@@ -3,15 +3,15 @@
 import assert from 'assert';
 import simple from '../../../js/util/Simple';
 
-describe('Simple', () => {
-  describe('#firstIntersect()', () => {
-    it('should return the first intersection if it exists', () => {
+describe('Simple', function () {
+  describe('#firstIntersect()', function () {
+    it('should return the first intersection if it exists', function () {
       assert.equal('view', simple.firstIntersect(['blah', 'blur', 2, 'view'], ['view']));
     });
-    it('should return -1 if no intersection', () => {
+    it('should return -1 if no intersection', function () {
       assert.equal(-1, simple.firstIntersect(['blah'], ['view']));
     });
-    it('should return -1 if both arrays are empty', () => {
+    it('should return -1 if both arrays are empty', function () {
       assert.equal(-1, simple.firstIntersect([], []));
     });
   });
